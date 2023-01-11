38dd0ca859ae7f5aee4d4e1d3b21852e85743558
jeanpierec <73913711+jeanpierec@users.noreply.github.com>
2023-01-10 19:10:02 -0500

    primera funcion

--- Backend/bck00_sample/src/Main.java
+++ Backend/bck00_sample/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int operacion = suma_pro(10, 30);
+        System.out.println(operacion);
+    }
+    // Una función algo... particular
+    public static void suma(int a, int b) {
+        int resultado = 0;
+        resultado = a + b;
+
+        System.out.println(resultado);
+    }
+    // Cambios el retorno de la función
+    public static int suma_pro(int a, int b) {
+        int resultado = 0;
+        resultado = a + b;
+
+        return resultado;
     }
 }
